292a20dcd468272ac043ea706dd016542e50cd1e
mak7 <zacevmax@andex.ru>
2020-06-22 13:50:35 +0400

    Webpack is installed and configured.
    Modules created, connected in three different ways. Optimized assembly.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import AppService from './modules/app.service'
+import { config } from './modules/config'
+import './modules/header.component'
+
+console.log('Config key:', config.key)
+
+const service = new AppService('Hello world!')
+service.log()
