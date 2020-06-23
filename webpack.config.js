ba07f1fc1cf97feed77a0b328b46711b21f5f233
mak7 <zacevmax@andex.ru>
2020-06-23 14:04:58 +0400

    Optimized CSS with OptimizeCssAssetsPlugin and MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -7,17 +10,26 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin({}),
+      // new UglifyJsPlugin({})
+    ]
+  },
   plugins: [
     new HTMLPlugin({
       filename: 'index.html',
       template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
     })
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
   }
